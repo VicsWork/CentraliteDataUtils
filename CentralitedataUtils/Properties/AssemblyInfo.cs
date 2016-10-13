c6cdd2845f8b79913b28a7d99196b417c1c3ad30
Victor Martin <victormartin@centralite.com>
2016-10-13 16:06:04 -0500

    Changed all Centrilitedatas

--- CentralitedataUtils/Properties/AssemblyInfo.cs
+++ CentralitedataUtils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CentralitedataUtils")]
+[assembly: AssemblyTitle("CentraliteDataUtils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CentralitedataUtils")]
+[assembly: AssemblyProduct("CentraliteDataUtils")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
