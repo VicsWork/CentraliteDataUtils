7b9fca54432be3e37ebdecb05d4d536bc997e9c1
Victor Martin <victormartin@centralite.com>
2016-10-14 16:06:15 -0500

    Added new BoardProcess table

--- CentralitedataUtils/Centralite.designer.cs
+++ CentralitedataUtils/Centralite.designer.cs
@@ -22,7 +22,7 @@ namespace CentraliteData
 	using System;
 	
 	
-	//[global::System.Data.Linq.Mapping.DatabaseAttribute(Name="ManufacturingStore_v2")]
+	//[global::System.Data.Linq.Mapping.DatabaseAttribute(Name="ManufacturingStore_RAD")]
 	public partial class CentraliteDataContext : System.Data.Linq.DataContext
 	{
 		
@@ -51,6 +51,9 @@ namespace CentraliteData
     partial void InsertInsightAdapter(InsightAdapter instance);
     partial void UpdateInsightAdapter(InsightAdapter instance);
     partial void DeleteInsightAdapter(InsightAdapter instance);
+    partial void InsertBoardProcess(BoardProcess instance);
+    partial void UpdateBoardProcess(BoardProcess instance);
+    partial void DeleteBoardProcess(BoardProcess instance);
     #endregion
 		
 		public CentraliteDataContext() : 
@@ -136,6 +139,14 @@ namespace CentraliteData
 			get
 			{
 				return this.GetTable<InsightAdapter>();
+			}
+		}
+		
+		public System.Data.Linq.Table<BoardProcess> BoardProcesses
+		{
+			get
+			{
+				return this.GetTable<BoardProcess>();
 			}
 		}
 	}
@@ -1599,6 +1610,92 @@ namespace CentraliteData
 					this._Location = value;
 					this.SendPropertyChanged("Location");
 					this.OnLocationChanged();
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.BoardProcess")]
+	public partial class BoardProcess : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _Id;
+		
+		private string _Name;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdChanging(int value);
+    partial void OnIdChanged();
+    partial void OnNameChanging(string value);
+    partial void OnNameChanged();
+    #endregion
+		
+		public BoardProcess()
+		{
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int Id
+		{
+			get
+			{
+				return this._Id;
+			}
+			set
+			{
+				if ((this._Id != value))
+				{
+					this.OnIdChanging(value);
+					this.SendPropertyChanging();
+					this._Id = value;
+					this.SendPropertyChanged("Id");
+					this.OnIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(50) NOT NULL", CanBeNull=false)]
+		public string Name
+		{
+			get
+			{
+				return this._Name;
+			}
+			set
+			{
+				if ((this._Name != value))
+				{
+					this.OnNameChanging(value);
+					this.SendPropertyChanging();
+					this._Name = value;
+					this.SendPropertyChanged("Name");
+					this.OnNameChanged();
 				}
 			}
 		}
